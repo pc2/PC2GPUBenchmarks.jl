05a9f9d61cb93e32ff81b5d4709a3babe355c3c0
Documenter.jl <documenter@juliadocs.github.io>
2022-02-13 17:34:01 +0100

    build based on 9ee69b2

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#PC2GPUBenchmarks.jl","page":"PC2GPUBenchmarks","title":"PC2GPUBenchmarks.jl","text":"","category":"section"},{"location":"","page":"PC2GPUBenchmarks","title":"PC2GPUBenchmarks","text":"Coming soon...","category":"page"}]
+}
